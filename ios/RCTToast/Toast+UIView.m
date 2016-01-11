185eb13a0949f925a9f4f7f7e0d44ea7da7a9802
sophiaWang611 <15088655203@126.com>
2016-01-11 21:06:23 +0800

    Update Toast+UIView.m
    do not show shadow

--- ios/RCTToast/Toast+UIView.m
+++ ios/RCTToast/Toast+UIView.m
@@ -24,7 +24,7 @@ static const NSTimeInterval CSToastFadeDuration = 0.2;
 static const CGFloat CSToastShadowOpacity       = 0.8;
 static const CGFloat CSToastShadowRadius        = 6.0;
 static const CGSize  CSToastShadowOffset        = { 4.0, 4.0 };
-static const BOOL    CSToastDisplayShadow       = YES;
+static const BOOL    CSToastDisplayShadow       = NO;
 
 // display duration and position
 static const NSString * CSToastDefaultPosition  = @"bottom";
